9f10b609e7215b9359a22590ffc36b52ae7b5863
Guilherme Souza <guilhermenunes098765@gmail.com>
2022-10-27 21:52:41 -0400

    Modificação 08

---
+++ Teste-copia.js
@@ -0,0 +1,40 @@
+var b1 = document.querySelector('#b1');
+var d42 = document.querySelector('#d42');
+
+b1.addEventListener('click', function(){
+
+  if(d42.style.display === 'block'){
+    d42.style.display = 'none';
+  }
+  else{
+    d42.style.display = 'block';
+  }
+});
+
+
+var b2 = document.querySelector('#b2');
+var d43 = document.querySelector('#d43');
+
+b2.addEventListener('click', function(){
+
+  if(d43.style.display === 'block'){
+    d43.style.display = 'none';
+  }
+  else{
+    d43.style.display = 'block';
+  }
+});
+
+
+var b3 = document.querySelector('#b3');
+var d44 = document.querySelector('#d44');
+
+b3.addEventListener('click', function(){
+
+  if(d44.style.display === 'block'){
+    d44.style.display = 'none';
+  }
+  else{
+    d44.style.display = 'block';
+  }
+});
